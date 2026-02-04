6de0a2058017987b1c575f06b8431ac57eb65532
manh20072001 <133735973+manh20072001@users.noreply.github.com>
2026-02-04 15:23:12 +0900

    Update service worker cache version and assets

--- sw.js
+++ sw.js
@@ -1,20 +1,15 @@
-const CACHE_NAME = 'shift-manager-v1';
+const CACHE_NAME = 'shift-manager-v3';
 const ASSETS = [
+  './',
   'index.html',
   'manifest.json',
   'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css'
 ];
 
-// Cài đặt Service Worker
 self.addEventListener('install', (e) => {
-  e.waitUntil(
-    caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS))
-  );
+  e.waitUntil(caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS)));
 });
 
-// Chạy offline
 self.addEventListener('fetch', (e) => {
-  e.respondWith(
-    caches.match(e.request).then((res) => res || fetch(e.request))
-  );
+  e.respondWith(caches.match(e.request).then((res) => res || fetch(e.request)));
 });
